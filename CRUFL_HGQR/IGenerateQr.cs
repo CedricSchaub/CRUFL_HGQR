23e85aa09bccb97f1d724036095e77e0e5cd536e
CedricSchaub <cedric.schaub@googlemail.com>
2020-10-12 16:14:07 +0200

    Init project

---
+++ CRUFL_HGQR/IGenerateQr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CRUFL_HGQR
+{
+    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsDual), Guid("5A9DA6EA-241E-47F8-8232-5BBB256EA7F4")]
+    public interface IGenerateQr
+    {
+        string generate(string value);
+    }
+}
---
+++ CRUFL_HGQR/QrCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CRUFL_HGQR
+{
+    [ComVisible(true), ClassInterface(ClassInterfaceType.None), GuidAttribute("064267D7-70FF-4D45-8D47-8A99C6604BB4")]
+    public class QrCode : IGenerateQr
+    {
+     
+        public string generate(string value)
+        {
+            return "Dieser Text ist von der C# Bibliothek " + value;
+        }
+    }
+}
